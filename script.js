9e050692d6b7e8fe7691d347f1aee642787d5abc
Dan Hackworth <danielhackworth515@gmail.com>
2023-09-27 19:06:39 -0400

    add framework for site and notes for content, publish framework

---
+++ script.js
@@ -0,0 +1,12 @@
+const burgerButton=document.getElementById('plain-burger');
+const dropDown = document.querySelector('.drop-menu');
+const menuExit = document.getElementById('menu-exit-button');
+burgerButton.addEventListener('click', function () {
+    dropDown.style.display = 'block';
+    
+});
+
+menuExit.addEventListener('click', function () {
+    dropDown.style.display = 'none';
+    
+});
